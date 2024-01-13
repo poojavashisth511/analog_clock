796c4dfd650ce146c2abdb3637eea57b73cff984
pooja <poojasharma62517@gmail.com>
2024-01-13 15:16:57 +0530

    add digitalclocl.js

---
+++ digital-clock.js
@@ -0,0 +1,26 @@
+let time = document.getElementById("time");
+// let timeformat = document.getElementById("timeformat");
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+      setInterval(showTime , 1000);
+});
+
+     const showTime = () =>{
+       
+       let date = new Date();
+
+      let hr= date.getHours();
+      let mins= date.getMinutes();
+      let secs= date.getSeconds();
+
+      hr = hr<10 ? `0${hr}`: hr;
+      mins=mins<10? `0${mins}`: mins;
+      secs= secs<10? `0${secs}`: secs;
+      
+
+      time.innerHTML = `${hr} : ${mins} : ${secs}`;
+
+    //  timeformat.innerHTML = hr>12 ? "PM" : "AM";
+    $("#timeformat").innerHTML= hr>12 ? "PM" : "AM";
+}
